9163b513b842c63871f8fa46139e1db5baa38cd0
Damian Siemieniec <damian.siemieniec@gmail.com>
2022-10-12 13:42:38 +0200

    define command published to exchange bound to custom queue test case

--- tests/Config/ConfigFactoryTest.php
+++ tests/Config/ConfigFactoryTest.php
@@ -212,4 +212,71 @@ class ConfigFactoryTest extends TestCase
         self::assertFalse($exchange->isDelayedActive());
         self::assertEquals(ExchangeType::DIRECT, $exchange->getType());
     }
+
+
+    public function testShouldDefineCommandPublishedToExchangeBoundToCustomQueue(): void
+    {
+        $data = [
+            'connections' => [
+                'default' => [
+                    'host' => 'localhost',
+                    'port' => 5672,
+                    'user' => 'guest',
+                    'password' => 'guest'
+                ]
+            ],
+            'queues' => [
+                'custom_queue_name' => [
+                    'name' => 'custom_queue'
+                ]
+            ],
+            'exchanges' => [
+                'test_exchange_name' => [
+                    'name' => 'test_exchange'
+                ]
+            ],
+            'bindings' => [
+                'test_binding' => [
+                    'queue' => 'custom_queue_name',
+                    'exchange' => 'test_exchange_name',
+                    'routing_key' => 'test_routing_key'
+                ]
+            ],
+            'commands' => [
+                'TestCommand' => [
+                    'publisher' => [
+                        'exchange' => [
+                            'name' => 'test_exchange_name',
+                            'routing_key' => 'test_routing_key'
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $config = (new ConfigFactory($data))->create();
+        $commandConfig = $config->getCommandConfig('TestCommand');
+        self::assertEquals('TestCommand', $commandConfig->getCommandClass());
+        self::assertEquals(DefaultCommandSerializer::class, $commandConfig->getSerializerClass());
+
+        $publisherConfig = $commandConfig->getPublisherConfig();
+        self::assertEquals('test_routing_key', $publisherConfig->getPublisherTarget()->getRoutingKey());
+        self::assertEquals('test_exchange', $publisherConfig->getPublisherTarget()->getExchange());
+
+        $publisherConnectionConfig = $publisherConfig->getConnection();
+        self::assertEquals('default', $publisherConnectionConfig->getName());
+        self::assertEquals('localhost', $publisherConnectionConfig->getHost());
+        self::assertEquals(5672, $publisherConnectionConfig->getPort());
+        self::assertEquals('guest', $publisherConnectionConfig->getUser());
+        self::assertEquals('guest', $publisherConnectionConfig->getPassword());
+        self::assertEquals('/', $publisherConnectionConfig->getVHost());
+
+        $binding = $config->getAllBindings()[0];
+        self::assertEquals($publisherConnectionConfig, $binding->getConnection());
+        self::assertEquals('custom_queue', $binding->getQueue()->getName());
+        $exchange = $binding->getExchange();
+        self::assertEquals('test_exchange', $exchange->getName());
+        self::assertFalse($exchange->isDelayedActive());
+        self::assertEquals(ExchangeType::DIRECT, $exchange->getType());
+    }
 }
