e414a44a1efa5c5a9f78cff94eac88bfeea0fee7
Damian Siemieniec <damian.siemieniec@gmail.com>
2022-10-12 11:33:52 +0200

    remove exception from long running consumer

--- src/Handler/LongRunningHandler.php
+++ src/Handler/LongRunningHandler.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Handler;
 
 use App\Command\LongRunningCommand;
-use Exception;
 
 class LongRunningHandler implements HandlerInterface
 {
@@ -16,9 +15,6 @@ class LongRunningHandler implements HandlerInterface
         echo \sprintf('Running for %d second(s)...', $seconds) . PHP_EOL;
         while ($seconds > 0) {
             \sleep(1);
-            if ($seconds === 10) {
-                throw new Exception();
-            }
             echo \sprintf('%d second(s) left', --$seconds) . PHP_EOL;
         }
         echo 'Finished' . PHP_EOL;
