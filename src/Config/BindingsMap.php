6751dd0058dfbed86748dcc4220c063c73755d7f
Damian Siemieniec <damian.siemieniec@gmail.com>
2022-10-13 13:18:53 +0200

    extract common abstract map class

--- src/Config/BindingsMap.php
+++ src/Config/BindingsMap.php
@@ -5,114 +5,31 @@ declare(strict_types=1);
 namespace App\Config;
 
 use App\Exception\MissingBindingException;
-use ArrayAccess;
-use Countable;
 use InvalidArgumentException;
-use Iterator;
 
-class BindingsMap implements ArrayAccess, Countable, Iterator
+class BindingsMap extends AbstractMap
 {
-    /** @var array<string, Binding> */
-    private array $bindings = [];
-    /** @var string[] */
-    private array $keys = [];
-    private int $position = 0;
-
     public function current(): Binding
     {
-        return $this->bindings[$this->keys[$this->position]];
+        return parent::current();
     }
 
-    public function next(): void
-    {
-        ++$this->position;
-    }
-
-    public function key(): string
-    {
-        return $this->keys[$this->position];
-    }
-
-    public function valid(): bool
-    {
-        return isset($this->keys[$this->position]);
-    }
-
-    public function rewind(): void
-    {
-        $this->position = 0;
-    }
-
-    public function count(): int
-    {
-        return \count($this->bindings);
-    }
-
-    /**
-     * @param string $offset
-     */
-    public function offsetExists(mixed $offset): bool
-    {
-        $this->assertOffsetType($offset);
-
-        return \array_key_exists($offset, $this->bindings);
-    }
-
-    /**
-     * @param string $offset
-     */
     public function offsetGet(mixed $offset): Binding
     {
-        $this->assertOffsetType($offset);
-
-        return $this->bindings[$offset]
-            ?? throw new MissingBindingException(\sprintf('Queue %s has not been defined', $offset));
+        return parent::offsetGet($offset);
     }
 
-    /**
-     * @param string $offset
-     * @param Binding $value
-     */
-    public function offsetSet(mixed $offset, mixed $value): void
-    {
-        $this->assertOffsetType($offset);
-        $this->assertValueType($value);
-
-        $this->bindings[$offset] = $value;
-        if (!\in_array($offset, $this->keys)) {
-            $this->keys[] = $offset;
-        }
-    }
-
-    /**
-     * @param string $offset
-     */
-    public function offsetUnset(mixed $offset): void
-    {
-        $this->assertOffsetType($offset);
-
-        unset($this->bindings[$offset]);
-        $key = \array_search($offset, $this->keys);
-        if ($key !== false) {
-            unset($this->keys[$key]);
-        }
-    }
-
-    private function assertOffsetType(mixed $offset): void
-    {
-        if (!\is_string($offset)) {
-            throw new InvalidArgumentException(
-                \sprintf('Offset must be of type string. %s given', \get_debug_type($offset))
-            );
-        }
-    }
-
-    private function assertValueType(mixed $value): void
+    protected function assertValueType(mixed $value): void
     {
         if (!($value instanceof Binding)) {
             throw new InvalidArgumentException(
                 \sprintf('Value must be of type %s. %s given', Binding::class, \get_debug_type($value))
             );
         }
+    }
+
+    protected function onMissingOffset(mixed $offset): mixed
+    {
+        throw new MissingBindingException(\sprintf('Binding %s has not been defined', $offset));
     }
 }
--- src/Config/QueuesMap.php
+++ src/Config/QueuesMap.php
@@ -5,114 +5,31 @@ declare(strict_types=1);
 namespace App\Config;
 
 use App\Exception\MissingQueueException;
-use ArrayAccess;
-use Countable;
 use InvalidArgumentException;
-use Iterator;
 
-class QueuesMap implements ArrayAccess, Countable, Iterator
+class QueuesMap extends AbstractMap
 {
-    /** @var array<string, Queue> */
-    private array $queues = [];
-    /** @var array<string> */
-    private array $keys = [];
-    private int $position = 0;
-
-    /**
-     * @param string $offset
-     */
-    public function offsetExists(mixed $offset): bool
-    {
-        $this->assertOffsetType($offset);
-
-        return \array_key_exists($offset, $this->queues);
-    }
-
-    /**
-     * @param string $offset
-     */
-    public function offsetGet(mixed $offset): Queue
-    {
-        $this->assertOffsetType($offset);
-
-        return $this->queues[$offset]
-            ?? throw new MissingQueueException(\sprintf('Queue %s has not been defined', $offset));
-    }
-
-    /**
-     * @param string $offset
-     * @param Queue $value
-     */
-    public function offsetSet(mixed $offset, mixed $value): void
-    {
-        $this->assertOffsetType($offset);
-        $this->assertValueType($value);
-
-        $this->queues[$offset] = $value;
-        if (!\in_array($offset, $this->keys)) {
-            $this->keys[] = $offset;
-        }
-    }
-
-    /**
-     * @param string $offset
-     */
-    public function offsetUnset(mixed $offset): void
-    {
-        $this->assertOffsetType($offset);
-
-        unset($this->queues[$offset]);
-        $key = \array_search($offset, $this->keys);
-        if ($key !== false) {
-            unset($this->keys[$key]);
-        }
-    }
-
-    public function count(): int
-    {
-        return \count($this->queues);
-    }
-
     public function current(): Queue
     {
-        return $this->queues[$this->keys[$this->position]];
+        return parent::current();
     }
 
-    public function next(): void
+    public function offsetGet(mixed $offset): Queue
     {
-        ++$this->position;
+        return parent::offsetGet($offset);
     }
 
-    public function key(): string
-    {
-        return $this->keys[$this->position];
-    }
-
-    public function valid(): bool
-    {
-        return isset($this->keys[$this->position]);
-    }
-
-    public function rewind(): void
-    {
-        $this->position = 0;
-    }
-
-    private function assertOffsetType(mixed $offset): void
-    {
-        if (!\is_string($offset)) {
-            throw new InvalidArgumentException(
-                \sprintf('Offset must be of type string. %s given', \get_debug_type($offset))
-            );
-        }
-    }
-
-    private function assertValueType(mixed $value): void
+    protected function assertValueType(mixed $value): void
     {
         if (!($value instanceof Queue)) {
             throw new InvalidArgumentException(
                 \sprintf('Value must be of type %s. %s given', Queue::class, \get_debug_type($value))
             );
         }
+    }
+
+    protected function onMissingOffset(mixed $offset): mixed
+    {
+        throw new MissingQueueException(\sprintf('Queue %s has not been defined', $offset));
     }
 }
