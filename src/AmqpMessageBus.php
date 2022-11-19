90662382002ec3067f4f109bb94809526af697da
Damian Siemieniec <damian.siemieniec@gmail.com>
2022-11-19 17:39:49 +0100

    fix bundle config

--- src/AmqpMessageBus.php
+++ src/AmqpMessageBus.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Siemieniec\AmqpMessageBus;
 
+use Exception;
 use Siemieniec\AmqpMessageBus\DependencyInjection\HandlerRegistryCompilerPass;
 use Siemieniec\AmqpMessageBus\DependencyInjection\MessageSerializerRegistryCompilerPass;
 use Siemieniec\AmqpMessageBus\Serializer\MessageSerializerInterface;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class AmqpMessageBus extends AbstractBundle
@@ -23,5 +27,18 @@ class AmqpMessageBus extends AbstractBundle
 
         $container->addCompilerPass(new HandlerRegistryCompilerPass());
         $container->addCompilerPass(new MessageSerializerRegistryCompilerPass());
+    }
+
+    /**
+     * @param array<int|string, mixed> $config
+     * @throws Exception
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $loader = new YamlFileLoader(
+            $builder,
+            new FileLocator(__DIR__ . '/../config')
+        );
+        $loader->load('services.yaml');
     }
 }
